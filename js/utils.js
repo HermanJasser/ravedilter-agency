7d58eff1fc6cab407759bd7bfa1ee3af9473ae71
ingebrigtfb <ingboe00998@stud.noroff.no>
2024-03-18 11:53:43 +0100

    header-mobile done

---
+++ js/utils.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Hamburger menu//
+
+window.onload = function () {
+    const menuBtns = document.querySelectorAll('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-nav');
+
+    menuBtns.forEach(function (menuBtn) {
+        menuBtn.addEventListener('click', function () {
+            menuBtn.classList.toggle('is-active');
+            mobileMenu.classList.toggle('is-active')
+            document.body.style.overflow = mobileMenu.classList.contains('is-active') ? 'hidden' : '';
+        });
+    });
+}
